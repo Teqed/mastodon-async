822c969ed634a1576b2d77d5c3db57f8524a5a29
Timothy Quilling <teqed@shatteredsky.net>
2024-01-19 14:32:59 -0500

    Allow embed_url to be missing

--- entities/src/card.rs
+++ entities/src/card.rs
@@ -37,8 +37,8 @@ pub struct Card {
     /// Height of preview, in pixels. When [`card_type`](Card::card_type) is `Link`, this is `0`.
     pub height: u64,
     /// Used for photo embeds, instead of custom html.
-    #[serde(default = "String::new")]
-    pub embed_url: String,
+    #[serde(default)]
+    pub embed_url: Option<String>,
     /// A hash computed by [the BlurHash algorithm](https://github.com/woltapp/blurhash),
     /// for generating colorful preview thumbnails when media has not been
     /// downloaded yet.
